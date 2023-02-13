027b2e009e00b3f8dc3db3e2e4d9eac28bcdd3ab
Kaitlyn Park <juyoung.toronto@gmail.com>
2023-02-12 22:12:21 -0500

    set up basic mockup template

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,13 @@
 <template>
-  <div></div>
+  <HomePage />
 </template>
 
 <script>
+import HomePage from "@/components/HomePage.vue";
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    
-  }
-}
+    HomePage,
+  },
+};
 </script>
