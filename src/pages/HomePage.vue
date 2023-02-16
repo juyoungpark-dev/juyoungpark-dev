aa0a69220a472661fe5fcfb132c3e0d0b7a84119
Kaitlyn Park <juyoung.toronto@gmail.com>
2023-02-16 14:58:26 -0500

    modulized main page

---
+++ src/pages/HomePage.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <NavigationBar />
+
+    <HomeMain />
+
+    <AboutMe />
+
+    <CarouselWidget />
+
+    <HomeFooter />
+  </div>
+</template>
+
+
+<script>
+import { defineComponent } from "vue";
+import NavigationBar from "@/components/NavigationBar.vue";
+import HomeMain from "@/components/HomeMain.vue";
+import CarouselWidget from "@/components/CarouselWidget.vue";
+import AboutMe from "@/components/AboutMe.vue";
+import HomeFooter from "@/components/HomeFooter.vue";
+export default defineComponent({
+  setup() {},
+  components: { NavigationBar, HomeMain, CarouselWidget, AboutMe, HomeFooter },
+});
+</script>
