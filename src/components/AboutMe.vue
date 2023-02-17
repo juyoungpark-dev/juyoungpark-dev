79f7cf7114693000961e987f2a50ad61ee82cd14
Kaitlyn Park <juyoung.toronto@gmail.com>
2023-02-16 19:54:01 -0500

    added about me and theme changed

--- src/components/AboutMe.vue
+++ src/components/AboutMe.vue
@@ -7,22 +7,50 @@
           <div class="col-md-7 col-md-offset-0 col-sm-10 col-sm-offset-1">
             <div class="text-center">
               <img
-                src="assets/img/dulau.jpg"
-                alt="Rounded Image"
-                class="img-rounded img-responsive img-dog"
+                src="assets/img/coder_cartoon.png"
+                alt="About Image"
+                class="img-circle img-responsive img-dog"
               />
             </div>
           </div>
           <div class="col-md-5 col-sm-12">
-            <h1 class="text-center">
-              About Juyoung Park
-
+            <h2>Hi 👋</h2>
+            <h1>
+              I'm Juyoung Park
               <small class="subtitle">Get to know me</small>
             </h1>
-            <hr />
-            <p>test</p>
-            <p>test 2</p>
-            <p>test 3</p>
+
+            <p>Learning and challenges are my passion.</p>
+            <p>
+              I had always dreamed to become a singer one day in my life. I love
+              the feeling to be on stage. I love to express myself in creative
+              ways. I love to inspire others.
+            </p>
+            <p>
+              One day, I thought &nbsp;
+              <span style="font-family: 'Courier New', Courier, monospace">
+                “This is not enough for me.”
+              </span>
+              .
+            </p>
+            <p>
+              Like everyone else, I had time to ponder what I wanted to do at
+              some point of my life. That was when I fell in love with
+              programming; I never imagined I would get close to technologies at
+              all. One day, I was trying to figure out what Python is for some
+              reason. And that was a whole new world. I realized that I am a
+              more logical person with a handful of creativity.
+            </p>
+            <p>
+              Writing a program is just like creating a piece of art. It is
+              creative, artistic, and even practical. It does not disappear.
+              Besides, did I tell you that my passion is learning and
+              challenging myself? Programming is perfect for me in this aspect.
+              It is very extensive that there is so much to learn. Learning
+              never ends in programming. It allows me to keep motivated. Most
+              importantly, seeing myself enjoying work not realizing it's
+              already past the work hour!!! 🤓
+            </p>
           </div>
         </div>
       </div>
